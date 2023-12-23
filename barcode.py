a793ede3f501ddd734c392d8bcf2888a663a42ee
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2023-12-23 02:59:47 -0330

    created read_file_header method

--- barcode.py
+++ barcode.py
@@ -1,3 +1,20 @@
+from collections import namedtuple
+
+HEADER_LENGTH = 21
+COMPLIANCE_INDICATOR = "@"
+
+FileHeader = namedtuple("FileHeader", [
+    "compliance_indicator",
+    "data_element_separator",
+    "record_separator",
+    "segment_terminator",
+    "file_type",
+    "issuer_identification_number",
+    "aamva_version_number",
+    "jurisdiction_version_number",
+    "number_of_entries",
+])
+
 def trim_to_indicator(_str: str, indicator: str) -> str:
     """Remove everything before the indicator, moving the indicator to the begining of the string"""
     if _str[0] != indicator:
@@ -7,3 +24,21 @@ def trim_to_indicator(_str: str, indicator: str) -> str:
             raise ValueError(f"Indicator \"{indicator}\" is missing")
         _str = _str[index:]
     return _str
+
+def read_file_header(file: str) -> FileHeader:
+    if len(file) < HEADER_LENGTH:
+        raise ValueError("file too short")
+    elif file[0] != COMPLIANCE_INDICATOR:
+        file = trim_to_indicator(file, COMPLIANCE_INDICATOR)
+    if file[4:9] != "ANSI ":
+        raise ValueError("file missing file type")
+    return FileHeader(
+            compliance_indicator=           str(file[0]),
+            data_element_separator=         str(file[1]),
+            record_separator=               str(file[2]),
+            segment_terminator=             str(file[3]),
+            file_type=                      str(file[4:9]),
+            issuer_identification_number=   int(file[9:15]),
+            aamva_version_number=           int(file[15:17]),
+            jurisdiction_version_number=    int(file[17:19]),
+            number_of_entries=              int(file[19:21]))
--- test_barcode.py
+++ test_barcode.py
@@ -4,10 +4,60 @@ import barcode
 
 class TestBarcode(unittest.TestCase):
     
+    def setUp(self) -> None:
+        self.example_file = """@
+
+ANSI 636000100002DL00410276ZV03180008DLDAQT64235789
+DCSSAMPLE
+DDEN
+DACMICHAEL
+DDFN
+DADJOHN
+DDGN
+DCUJR
+DCAD
+DCBK
+DCDPH
+DBD06062019
+DBB06061986
+DBA12102024
+DBC1
+DAU068 in
+DAYBRO
+DAG2300 WEST BROAD STREET
+DAIRICHMOND
+DAJVA
+DAK232690000 
+DCF2424244747474786102204
+DCGUSA
+DCK123456789
+DDAF
+DDB06062018
+DDC06062020
+DDD1
+ZVZVA01
+
+"""
+        return super().setUp()
+    
     def test_trim_to_indicator(self):
         self.assertEqual(barcode.trim_to_indicator("@After Indicator", "@"), "@After Indicator")
         self.assertEqual(barcode.trim_to_indicator("Before Indicator@After Indicator", "@"), "@After Indicator")
         self.assertRaises(ValueError, barcode.trim_to_indicator, "no indicator", "@")
+    
+    def test_read_file_header(self):
+        file_header = barcode.read_file_header(self.example_file)
+        
+        self.assertIsInstance(file_header, barcode.FileHeader)
+        self.assertEqual(file_header.compliance_indicator, "@")
+        self.assertEqual(file_header.data_element_separator, "\n")
+        self.assertEqual(file_header.record_separator, "")
+        self.assertEqual(file_header.segment_terminator, "\n")
+        self.assertEqual(file_header.file_type, "ANSI ")
+        self.assertEqual(file_header.aamva_version_number, 10)
+        self.assertEqual(file_header.issuer_identification_number, 636000)
+        self.assertEqual(file_header.jurisdiction_version_number, 0)
+        self.assertEqual(file_header.number_of_entries, 2)
 
 
 if __name__ == "__main__":
