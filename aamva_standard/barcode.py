345466125fcbdbe469ccce92a30e78dfbb7587db
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2024-01-20 19:00:22 -0330

    modify docstring of header_length

--- aamva_standard/barcode.py
+++ aamva_standard/barcode.py
@@ -3,7 +3,7 @@ DESIGNATOR_LENGTH = 10
 
 
 def header_length(version: int) -> int:
-    """Returns the fixed header length based on the version"""
+    """Returns the header length for the given version"""
     return 19 if version < 2 else 21
 
 
