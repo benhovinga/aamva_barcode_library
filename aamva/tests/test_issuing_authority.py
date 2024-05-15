efc999ac877e50bfcea4e1d0bd77d04555452366
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2024-05-15 14:33:37 -0230

    moved test cases to test class

--- aamva/tests/test_issuing_authority.py
+++ aamva/tests/test_issuing_authority.py
@@ -2,7 +2,6 @@ import pytest
 
 import aamva.issuing_authority as issuing_authority
 
-
 issuing_authority_testdata = (
     (100001, "Test Jurisdiction 1", "T1", "Canada"),
     (100002, "Test Jurisdiction 2", "T2", "USA"))
@@ -17,12 +16,12 @@ def fake_authority_list():
     issuing_authority.ISSUING_AUTHORITIES = saved_authorities
 
 
-@pytest.mark.parametrize("issuer_id, jurisdiction, abbr, country", issuing_authority_testdata)
-def test_can_get_authority_by_id(fake_authority_list, issuer_id, jurisdiction, abbr, country):
-    assert issuing_authority.get_authority_by_id(issuer_id) == (issuer_id, jurisdiction, abbr, country)
-    assert type(issuing_authority.get_authority_by_id(issuer_id)) == issuing_authority.IssuingAuthority
+class TestGetAuthorityByIdFunction:
+    @pytest.mark.parametrize("issuer_id, jurisdiction, abbr, country", issuing_authority_testdata, ids=(0, 1))
+    def test_should_successfully_return_issuing_authority_tuple(self, fake_authority_list, issuer_id, jurisdiction, abbr, country):
+        assert issuing_authority.get_authority_by_id(issuer_id) == (issuer_id, jurisdiction, abbr, country)
+        assert type(issuing_authority.get_authority_by_id(issuer_id)) == issuing_authority.IssuingAuthority
 
-
-def test_should_raise_value_error_when_id_not_found():
-    with pytest.raises(ValueError, match="not found"):
-        issuing_authority.get_authority_by_id(1)
+    def test_should_raise_value_error_when_id_not_found(self):
+        with pytest.raises(ValueError, match="not found"):
+            issuing_authority.get_authority_by_id(1)
