55fac10f7cd73d1425e504352e4e74d2c00f7747
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2025-02-13 18:14:35 -0330

    wip

---
+++ aamva/profile.py
@@ -0,0 +1,13 @@
+from datetime import date
+from typing import Union, Mapping
+
+from barcode import BarcodeFile
+from dates import get_date_format
+from issuing_authority import get_authority_by_id
+
+
+def open_barcode_file(barcode_file: BarcodeFile) -> Mapping[str, Union[str, int, date]]:
+    aamva_version = barcode_file.header.aamva_version
+    issuing_authority = get_authority_by_id(barcode_file.header.issuer_id)
+    country = issuing_authority.country
+    date_format = get_date_format(aamva_version, country)
