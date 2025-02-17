b9d6d436af1c678f051223307aff619302226e00
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2025-02-17 09:32:16 -0330

    Add DateFormat type

--- aamva/dates.py
+++ aamva/dates.py
@@ -1,10 +1,13 @@
 from datetime import datetime, date
+from typing import Literal
 
-ISO_FORMAT = "%Y%m%d"
-IMPERIAL_FORMAT = "%m%d%Y"
+DateFormat = Literal["%Y%m%d", "%m%d%Y"]
+
+ISO_FORMAT: DateFormat = "%Y%m%d"
+IMPERIAL_FORMAT: DateFormat = "%m%d%Y"
 
 
-def country_date_format(country: str) -> str:
+def country_date_format(country: str) -> DateFormat:
     country = country.upper()
     if country == "CANADA":
         return ISO_FORMAT
@@ -15,12 +18,12 @@ def country_date_format(country: str) -> str:
     raise ValueError("Provided country is not supported.")
 
 
-def get_date_format(aamva_version: int, country: str) -> str:
+def get_date_format(aamva_version: int, country: str) -> DateFormat:
     # AAMVA Version 3 introduced ISO date format option, prior versions only used Imperial format.
     return IMPERIAL_FORMAT if aamva_version < 3 else country_date_format(country)
 
 
-def parse_date(date_string: str, format: str) -> date:
+def parse_date(date_string: str, format: DateFormat) -> date:
     try:
         return datetime.strptime(date_string, format).date()
     except ValueError:
